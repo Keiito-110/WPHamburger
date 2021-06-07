700ed39a8d627c1b6e243a25a7a2f5618b03e306
Kei <keita_itojpn@yahoo.co.jp>
2021-06-08 07:54:32 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+        <main class="l-main--top c-figure--top">
+            <h1 class="c-title--main">ダミーサイト</h1>
+        </main>
+
+        <article class="l-main--article">
+ 
+            <li class="l-main--article_takeout c-figure--article_takeout">
+                <h2 class="c-title--article"><a href="#">Take Out</a></h2>
+                <div class="l-main--article_comment">
+                    <dl class="c-title--article_comment">
+                        <dt>小見出しが入ります</dt>
+                        <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
+                    </dl>
+                    <dl class="c-title--article_comment">
+                        <dt>小見出しが入ります</dt>
+                        <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
+                    </dl>
+                </div>    
+            </li>
+    
+            <li class="l-main--article_eatin c-figure--article_eatin">
+                <h2 class="c-title--article"><a href="#">Eat In</a></h2> 
+                <div class="l-main--article_comment">
+                    <dl class="c-title--article_comment">
+                        <dt>小見出しが入ります</dt>
+                        <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
+                    </dl>
+                    <dl class="c-title--article_comment">
+                        <dt>小見出しが入ります</dt>
+                        <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
+                    </dl>
+                </div>    
+            </li>
+        </article>
+
+
+        <nav class="c-figure--access">
+            <div class="l-title--access c-title--access">
+                <h2>見出しが入ります</h2>
+                <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+            </div>
+        </nav>
+
+    </div>
+
+    <?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer(); ?>
+<?php wp_footer();?>
+
+</body>
+
+</html>
